3f6369d9d14f1b9631b75191d15e58b748daa34c
alain <shemaalain10@gmail.com>
2024-04-14 11:10:58 +0200

    modified image

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -19,30 +19,31 @@ const variants = {
 const Contact = () => {
   return (
     <motion.div className="contact" variants={variants} initial="initial" animate="animate">
-      <motion.div className="textContainer" variants={variants}>
-        <motion.h1 variants={variants}>Let's work together</motion.h1>
-        <motion.div className="item" variants={variants}>
-          <h2>Mail</h2>
-          <span>shemaalain10@gmail.com</span>
-        </motion.div>
-        <div className="item" variants={variants}>
-          <h2>Address</h2>
-          <span>Kigali/ Kacyiru</span>
-        </div>
-        <div className="item" variants={variants}>
-          <h2>Phone</h2>
-          <span>+250 781 216 497</span>
-        </div>
+    <motion.div className="textContainer" variants={variants}>
+      <motion.h1 variants={variants}>Let's work together</motion.h1>
+      <motion.div className="item" variants={variants}>
+        <h2>Mail</h2>
+        <span>shemaalain10@gmail.com</span>
       </motion.div>
-      <motion.div className="formContainer" variants={variants}>
-        <form>
-          <input type="text" required placeholder="Name" />
-          <input type="email" required placeholder="Email" />
-          <textarea rows={8} placeholder="Message" />
-          <button>Submit</button>
-        </form>
-      </motion.div>
+      <div className="item" variants={variants}>
+        <h2>Address</h2>
+        <span>Kigali/ Kacyiru</span>
+      </div>
+      <div className="item" variants={variants}>
+        <h2>Phone</h2>
+        <span>+250 781 216 497</span>
+      </div>
     </motion.div>
+    <motion.div className="formContainer" variants={variants}>
+      <form>
+        <input type="text" required placeholder="Name" />
+        <input type="email" required placeholder="Email" />
+        <textarea rows={8} placeholder="Message" />
+        <button>Submit</button>
+      </form>
+    </motion.div>
+  </motion.div>
+  
 
   )
 }
